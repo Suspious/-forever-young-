73e805cad92c83a2dc6f413e6ffef610b143fb94
Suspious <59746897+Suspious@users.noreply.github.com>
2021-09-15 12:04:32 +0200

    robot arm

---
+++ ex4.py
@@ -0,0 +1,38 @@
+from RobotArm import RobotArm
+robotArm = RobotArm('exercise 4')
+robotArm.grab();
+robotArm.moveRight();
+robotArm.moveRight();
+robotArm.drop();
+robotArm.moveLeft();
+robotArm.moveLeft();
+robotArm.grab();
+robotArm.moveRight();
+robotArm.moveRight();
+robotArm.drop();
+robotArm.moveLeft();
+robotArm.moveLeft();
+robotArm.grab();
+robotArm.moveRight();
+robotArm.drop();
+robotArm.moveRight();
+robotArm.grab();
+robotArm.moveLeft();
+robotArm.drop();
+robotArm.moveRight();
+robotArm.grab();
+robotArm.moveLeft();
+robotArm.drop();
+robotArm.wait();
+
+#made by anthony
+
+
+
+
+
+
+
+
+
+
---
+++ example.py
@@ -0,0 +1,17 @@
+from RobotArm import RobotArm
+
+#Jouw python instructies zet je vanaf hier:
+
+
+robotArm = RobotArm('exercise 1')
+robotArm.moveRight();
+robotArm.grab();
+robotArm.moveLeft();
+robotArm.drop();
+
+ 
+
+
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
